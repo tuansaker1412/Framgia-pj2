1f98f9b9e8db47db4f12463d6892349741ed1bb2
Ngoc Tuan <tuansaker1412@gmail.com>
2017-06-29 16:28:32 +0700

    done sign-up

--- app/assets/javascripts/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.modal').modal('');
-  $('#modal1').modal('open');
-  $('#modal1').modal('close');
-  $('#modal2').modal('open');
-  $('#modal2').modal('close');
-});
