67d62f0965bb28ff097aee6bbdc0bb8379edbb26
Ngoc Tuan <tuansaker1412@gmail.com>
2017-06-29 13:19:44 +0700

    done sign-up, sign-in, log-out, update-profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get "about", to: "static_pages#about"
   get "contact", to: "static_pages#contact"
   devise_for :users
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    post "login", to: "devise/sessions#create"
+    get "signup", to: "devise/registrations#new"
+    post "signup", to: "devise/registrations#create"
+    get "edit", to: "devise/registrations#edit"
+    put "edit", to: "devise/registrations#update"
+  end
 end
